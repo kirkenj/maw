391acf69515ddf82f64d48b362af83945062626a
KoSTyA-bel <kostyafedorakin@gmail.com>
2020-11-06 02:03:09 +0300

    лщлщ

---
+++ OOP4/models/A1Logic.cs
@@ -0,0 +1,16 @@
+namespace OOP4.models
+{
+    public static class A1Logic
+    {
+        public static bool IsSitesOfTriange(double a, double b, double c)
+        {
+            if ((a + b > c) && (b + c > a) && (a + c > b)){
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
