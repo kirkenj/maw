b3dd76ddfee31a4bcaa48fb7c6d719b3e7a94cd4
KoSTyA-bel <kostyafedorakin@gmail.com>
2020-11-06 01:43:45 +0300

    вроде как должно работать

--- OOP4/models/A2Logic.cs
+++ OOP4/models/A2Logic.cs
@@ -4,28 +4,34 @@ namespace OOP4.models
     {
         public static bool IsVowel(char c)
         {
-            switch (c.ToLower())
+            switch (c)
             {
-                case "a":
-                    return True;
-                    break;
-                case "e":
-                    return True;
-                    break;
-                case "i":
-                    return True;
-                    break;
-                case "o":
-                    return True;
-                    break;
-                case "u":
-                    return True;
-                    break;
-                case "y":
-                    return True;
-                    break;
+                case 'a':
+                    return true;
+                case 'e':
+                    return true;
+                case 'i':
+                    return true;
+                case 'o':
+                    return true;
+                case 'u':
+                    return true;
+                case 'y':
+                    return true;
+                case 'A':
+                    return true;
+                case 'E':
+                    return true;
+                case 'I':
+                    return true;
+                case 'O':
+                    return true;
+                case 'U':
+                    return true;
+                case 'Y':
+                    return true;
                 default:
-                    return False;
+                    return false;
             }
         }
     }
